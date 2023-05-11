eb89a6ba2a21c90b3ed68b0054c02b6b8cfae9ee
saidBoudad <saidBoudad35@gmail.com>
2023-05-11 17:14:44 +0100

    exercise 9

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -1,5 +1,6 @@
 package io.javabrains.reactiveworkshop;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.SignalType;
 
 import java.io.IOException;
@@ -10,26 +11,41 @@ public class Exercise9 {
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumbersFlux()
-
-        // Print size of intNumbersFlux after the last item returns
-        ReactiveSources.intNumbersFluxWithException()
+        Flux.just(1, 2, 3)
+                .map(i -> i * 2)
+                .map(i -> {
+                    if (i == 4) {
+                        throw new RuntimeException();
+                    }
+                    return i;
+                })
                 .doFinally(signalType -> {
                     if (signalType == SignalType.ON_COMPLETE) {
-                        System.out.println("Done!");
+                        System.out.println("Flux completed successfully");
                     } else if (signalType == SignalType.ON_ERROR) {
-                        {
-                            System.out.println("Error!");
-                        }
+                        System.out.println("Flux completed with an error");
                     }
-
                 })
-                .subscribe(s);
+                .subscribe(System.out::println);
 
-        // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+        // Print size of intNumbersFlux after the last item returns
+        // this methods convert Flux to Mono  count()
+        ReactiveSources.intNumbersFlux()
+                .count()
+                .subscribe(System.out::println);
+
+
+        // Collect all items of intNumbersFlux into a single list and print
+        // this methods convert Flux to Mono  collectList()
+        ReactiveSources.intNumbersFlux()
+                .collectList()
+                .subscribe(System.out::println);
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux()
+                .buffer(2)
+                .map(list -> list.get(0) + list.get(1))
+                .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
