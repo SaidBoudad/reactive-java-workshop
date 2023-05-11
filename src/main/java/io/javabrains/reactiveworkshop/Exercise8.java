9e0819a94b8bdae62801d96fd97962e15950d11e
saidBoudad <saidBoudad35@gmail.com>
2023-05-11 16:27:56 +0100

    exercise 8

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -12,14 +12,24 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+//        ReactiveSources.intNumbersFluxWithException()
+//                .subscribe(num -> System.out.println(num),
+//                        ex -> System.out.println("An error happened"));
+        // or by an alternative way
+//        ReactiveSources.intNumbersFluxWithException()
+//                .doOnError(er -> System.out.println("Error !!!" + er.getMessage())) //the error still occurs
+//                .subscribe(System.out::println);
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorContinue((e, item) -> System.out.println("Error !! " + e.getMessage())) //continue the Flux not ending it
+                .subscribe(System.out::println);
 
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume(e -> Flux.just(-1, -2)) //switch to the given new flux or mono
+                .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
